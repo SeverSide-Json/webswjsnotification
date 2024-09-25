42fd6d0e256245e96f6fcf323c4d2cc5e0784059
SeverSide-Json <dangminhtienfun1994@gmail.com>
2024-09-26 01:02:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const SHEET_ID = '1Zebh-8FerNoGurfyqQP-pcSFFT_CXAcnh1I-GFHpv_c';
 const SHEET_TITLE = 'Sheet3';
-const SHEET_RANGE = 'A:F';
+const SHEET_RANGE = 'A:I';
 const POLL_INTERVAL = 1000; // 1 second
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyUHh84KAvV27u15X8rwGHq36PM-x6Dy5hgPLAEwKqIyg8kC5IQ3NJVVc05p_hp0XE5aQ/exec'
 
